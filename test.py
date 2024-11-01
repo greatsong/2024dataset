d5c0496e345f4d6a68dead15523096e0d929a8ca
송석리(Song Sukree) <greatsong21@gmail.com>
2024-11-01 11:10:27 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+st.title("MBTI 성향 및 궁합 테스트")
+
+name = st.text_input("이름을 입력하세요:")
+mbti = st.selectbox("MBTI 타입을 선택하세요:", ['ENTJ','ISTJ','INTJ'])
+
+if name and mbti:
+    st.subheader(f"{name}님의 MBTI 성향")
+    st.write(mbti+'스러운 성격입니다')
